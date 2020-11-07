ec4a0a1d55ae834c0b18d79c456c4d40db27491f
Linh <linh9920duy@gmail.com>
2020-11-07 19:55:16 +0700

    add student folder & files

---
+++ Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/HOCSINHDATA/HOCSINHDATA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+using System.Data.ProviderBase;
+namespace Quan_Ly_HS_GV_THPT.HOCSINHDATA
+{
+    public class HOCSINHDATA:APP_DATA.DATA
+    {
+        public HOCSINHDATA()
+        {
+            base.path = "select *from HOCSINH ";
+        }
+        public void GAN_HS(List<String> HS)
+        {
+            cmd.Parameters.AddWithValue("@ID", HS[0]);
+            cmd.Parameters.AddWithValue("@NAME", HS[1]);
+            cmd.Parameters.AddWithValue("@CLASSID", HS[5]);
+            DateTime a = Convert.ToDateTime(HS[3]);
+            cmd.Parameters.AddWithValue("@DATE", a);
+            cmd.Parameters.AddWithValue("@SEX", HS[3]);
+            cmd.Parameters.AddWithValue("@DIACHI", HS[4]);
+            
+        }
+
+        
+}
---
+++ Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/HOCSINHDATA/frmHocSinh.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTHPT.HOCSINHDATA
+{
+    public partial class frmHocSinh : Form
+    {
+        List<string> HS;
+        HOCSINHDATA hocsinh = new HOCSINHDATA();
+        DataTable dt = new DataTable();
+        public frmHocSinh()
+        {
+            InitializeComponent();
+
+        }
+        private void ADD_LIST()
+        {
+            HS.Clear();
+            HS.Add(tbx_ID.Text.Trim());
+            HS.Add(tbx_NAME.Text.Trim());
+            HS.Add(tbx_CLASSID.Text.Trim());
+            HS.Add(tbx_DATE.Text.Trim());
+            HS.Add(tbx_SEX.Text.Trim());
+            HS.Add(tbx_HOME.Text.Trim());
+        }
+        private void LOADHS()
+        {
+            dt = hocsinh.SHOWDATA();
+            DGVHS.DataSource = dt;
+            DGVHS.AutoResizeColumns();
+        }
+
+        private void HOCSINH_Load(object sender, EventArgs e)
+        {
+            HS = new List<string>();
+
+            LOADHS();
+
+            for (int i = 1; i < DGVHS.ColumnCount; i++)
+            {
+                DGVHS.Columns[i].ReadOnly = true;
+            }
+        }
+        
+}
+        
+
