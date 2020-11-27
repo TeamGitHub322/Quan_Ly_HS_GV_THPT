65f47dd80166a1f5068ecc387d6aec0e91e9c8b7
Nguyễn Đình Việt <70870418+Nguyen-Dinh-Viet@users.noreply.github.com>
2020-11-27 10:12:00 +0700

    Update Form1.designer.cs

--- Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/Form1.designer.cs
+++ Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/Form1.designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Quan_Ly_HS_GV_THPT
 {
-     partial class frm_Start
+     partial class Form1
      {
           /// <summary>
           /// Required designer variable.
@@ -28,92 +28,13 @@
           /// </summary>
           private void InitializeComponent()
           {
-               System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Start));
-               this.button1 = new System.Windows.Forms.Button();
-               this.button2 = new System.Windows.Forms.Button();
-               this.button3 = new System.Windows.Forms.Button();
-               this.button4 = new System.Windows.Forms.Button();
-               this.SuspendLayout();
-               // 
-               // button1
-               // 
-               this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-               this.button1.Image = global::Quan_Ly_HS_GV_THPT.Properties.Resources.classroom;
-               this.button1.Location = new System.Drawing.Point(374, 1);
-               this.button1.Name = "button1";
-               this.button1.Size = new System.Drawing.Size(130, 153);
-               this.button1.TabIndex = 0;
-               this.button1.Text = "LỚP";
-               this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-               this.button1.UseVisualStyleBackColor = true;
-               this.button1.Click += new System.EventHandler(this.button1_Click);
-               // 
-               // button2
-               // 
-               this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-               this.button2.Image = global::Quan_Ly_HS_GV_THPT.Properties.Resources.student_2;
-               this.button2.Location = new System.Drawing.Point(102, 1);
-               this.button2.Name = "button2";
-               this.button2.Size = new System.Drawing.Size(130, 153);
-               this.button2.TabIndex = 1;
-               this.button2.Text = "HỌC SINH";
-               this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-               this.button2.UseVisualStyleBackColor = true;
-               this.button2.Click += new System.EventHandler(this.button2_Click);
-               // 
-               // button3
-               // 
-               this.button3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-               this.button3.Image = global::Quan_Ly_HS_GV_THPT.Properties.Resources.teacher;
-               this.button3.Location = new System.Drawing.Point(238, 1);
-               this.button3.Name = "button3";
-               this.button3.Size = new System.Drawing.Size(130, 153);
-               this.button3.TabIndex = 2;
-               this.button3.Text = "GIÁO VIÊN";
-               this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-               this.button3.UseVisualStyleBackColor = true;
-               this.button3.Click += new System.EventHandler(this.button3_Click);
-               // 
-               // button4
-               // 
-               this.button4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-               this.button4.Image = global::Quan_Ly_HS_GV_THPT.Properties.Resources.guide;
-               this.button4.Location = new System.Drawing.Point(510, 1);
-               this.button4.Name = "button4";
-               this.button4.Size = new System.Drawing.Size(130, 153);
-               this.button4.TabIndex = 3;
-               this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-               this.button4.UseVisualStyleBackColor = true;
-               this.button4.Click += new System.EventHandler(this.button4_Click);
-               // 
-               // frm_Start
-               // 
-               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+               this.components = new System.ComponentModel.Container();
                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-               this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-               this.BackgroundImage = global::Quan_Ly_HS_GV_THPT.Properties.Resources.QuanLyTHPT;
-               this.ClientSize = new System.Drawing.Size(751, 498);
-               this.Controls.Add(this.button4);
-               this.Controls.Add(this.button3);
-               this.Controls.Add(this.button2);
-               this.Controls.Add(this.button1);
-               this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-               this.Name = "frm_Start";
-               this.Text = "Quản lý THPT";
-               this.Load += new System.EventHandler(this.frm_Start_Load);
-               this.ResumeLayout(false);
-
+               this.ClientSize = new System.Drawing.Size(800, 450);
+               this.Text = "Form1";
           }
 
           #endregion
-
-          private System.Windows.Forms.Button button1;
-          private System.Windows.Forms.Button button2;
-          private System.Windows.Forms.Button button3;
-          private System.Windows.Forms.Button button4;
-
-
-
      }
 }
 
