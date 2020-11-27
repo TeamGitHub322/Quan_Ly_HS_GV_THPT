150c25fa579c1fb64f15c07e4e035f18f9149d22
Nguyễn Đình Việt <70870418+Nguyen-Dinh-Viet@users.noreply.github.com>
2020-11-27 14:34:08 +0700

    Update GIAOVIENDATA.cs

--- Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/GIAOVIENDATA/GIAOVIENDATA.cs
+++ Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/GIAOVIENDATA/GIAOVIENDATA.cs
@@ -10,86 +10,86 @@ using Quan_Ly_HS_GV_THPT.APP_DATA;
 
 namespace Quan_Ly_HS_GV_THPT.GIAOVIENDATA
 {
-   public class GIAOVIENDATA: DATA
-    {
-         public GIAOVIENDATA()
-        {
-            base.path = "select *from GIAOVIEN ";
-        }
-        public void GAN_GV(List<String> GV)
-        {
-            cmd.Parameters.AddWithValue("@ID", GV[0]);
-            cmd.Parameters.AddWithValue("@NAME", GV[1]);
-            cmd.Parameters.AddWithValue("@SDT", GV[2]);
-            cmd.Parameters.AddWithValue("@EMAIL", GV[3]);
-            cmd.Parameters.AddWithValue("@SEX", GV[4]);
-            cmd.Parameters.AddWithValue("@HOCHAM", GV[5]);
-            cmd.Parameters.AddWithValue("@HOCVI", GV[6]);
-            DateTime a=Convert.ToDateTime(GV[7]);
-            cmd.Parameters.AddWithValue("@DATE",a);
-        }
+     public class GIAOVIENDATA : DATA
+     {
+          public GIAOVIENDATA()
+          {
+               base.path = "select * from GIAOVIEN ";
+          }
+          public void GAN_GV(List<String> GV)
+          {
+               cmd.Parameters.AddWithValue("@ID", GV[0]);
+               cmd.Parameters.AddWithValue("@NAME", GV[1]);
+               cmd.Parameters.AddWithValue("@SDT", GV[2]);
+               cmd.Parameters.AddWithValue("@EMAIL", GV[3]);
+               cmd.Parameters.AddWithValue("@SEX", GV[4]);
+               cmd.Parameters.AddWithValue("@HOCHAM", GV[5]);
+               cmd.Parameters.AddWithValue("@HOCVI", GV[6]);
+               DateTime a = Convert.ToDateTime(GV[7]);
+               cmd.Parameters.AddWithValue("@DATE", a);
+          }
 
-        public void InsertGV(List<string> GV)
-        {
-            CHECK = 0;
-            base.sql = "INSERTGIAOVIEN";
-            SP_DATA();
-            GAN_GV(GV);
-            check();
+          public void InsertGV(List<string> GV)
+          {
+               CHECK = 0;
+               base.sql = "INSERTGIAOVIEN";
+               SP_DATA();
+               GAN_GV(GV);
+               check();
 
-        }
-        public void UpdateGV(List<String> GV)
-        {
-            CHECK = 0;
-            base.sql = "UPDATEGIAOVIEN";
-            SP_DATA();
-            GAN_GV(GV);
-            check();
+          }
+          public void UpdateGV(List<String> GV)
+          {
+               CHECK = 0;
+               base.sql = "UPDATEGIAOVIEN";
+               SP_DATA();
+               GAN_GV(GV);
+               check();
 
-        }
-        public void DeleteGV(string ID)
-        {
-            base.sql = "DELETEGIAOVIEN";
-            SP_DATA();
-            cmd.Parameters.AddWithValue("@ID", ID);
-            cmd.ExecuteNonQuery();
-            cmd.Dispose();
-            con.Close();
-        }
-
-       
-        public DataTable TK_THONGTIN_MA(string LISTHS)
-        {
-            base.sql = "TK_THONGTIN_MA";
-
-            SqlDataAdapter sql1 = new SqlDataAdapter(sql, con);
-            sql1.SelectCommand.CommandType = CommandType.StoredProcedure;
-            sql1.SelectCommand.Parameters.AddWithValue("@ID", LISTHS);
-            sql1.SelectCommand.Parameters.Add("@CHECK", SqlDbType.Int).Direction = ParameterDirection.Output;
-            sql1.SelectCommand.ExecuteNonQuery();
-            CHECK = Convert.ToInt16(sql1.SelectCommand.Parameters["@CHECK"].Value);
-            DataTable dt = new DataTable();
-            sql1.Fill(dt);
-            sql1.Dispose();
-            return dt;
+          }
+          public void DeleteGV(string ID)
+          {
+               base.sql = "DELETEGIAOVIEN";
+               SP_DATA();
+               cmd.Parameters.AddWithValue("@ID", ID);
+               cmd.ExecuteNonQuery();
+               cmd.Dispose();
+               con.Close();
+          }
 
 
-        }
-        public DataTable TK_THONGTIN_TEN(string LISTGV)
-        {
-            base.sql = "TK_THONGTIN_TEN";
+          public DataTable TK_THONGTIN_MA(string LISTHS)
+          {
+               base.sql = "TK_THONGTIN_MA";
 
-            SqlDataAdapter sql1 = new SqlDataAdapter(sql, con);
-            sql1.SelectCommand.CommandType = CommandType.StoredProcedure;
-            sql1.SelectCommand.Parameters.AddWithValue("@NAME", LISTGV);
-            sql1.SelectCommand.Parameters.Add("@CHECK", SqlDbType.Int).Direction = ParameterDirection.Output;
-            sql1.SelectCommand.ExecuteNonQuery();
-            CHECK = Convert.ToInt16(sql1.SelectCommand.Parameters["@CHECK"].Value);
-            DataTable dt = new DataTable();
-            sql1.Fill(dt);
-            sql1.Dispose();
-            return dt;
+               SqlDataAdapter sql1 = new SqlDataAdapter(sql, con);
+               sql1.SelectCommand.CommandType = CommandType.StoredProcedure;
+               sql1.SelectCommand.Parameters.AddWithValue("@ID", LISTHS);
+               sql1.SelectCommand.Parameters.Add("@CHECK", SqlDbType.Int).Direction = ParameterDirection.Output;
+               sql1.SelectCommand.ExecuteNonQuery();
+               CHECK = Convert.ToInt16(sql1.SelectCommand.Parameters["@CHECK"].Value);
+               DataTable dt = new DataTable();
+               sql1.Fill(dt);
+               sql1.Dispose();
+               return dt;
 
-    }
-}
+
+          }
+          public DataTable TK_THONGTIN_TEN(string LISTGV)
+          {
+               base.sql = "TK_THONGTIN_TEN";
+
+               SqlDataAdapter sql1 = new SqlDataAdapter(sql, con);
+               sql1.SelectCommand.CommandType = CommandType.StoredProcedure;
+               sql1.SelectCommand.Parameters.AddWithValue("@NAME", LISTGV);
+               sql1.SelectCommand.Parameters.Add("@CHECK", SqlDbType.Int).Direction = ParameterDirection.Output;
+               sql1.SelectCommand.ExecuteNonQuery();
+               CHECK = Convert.ToInt16(sql1.SelectCommand.Parameters["@CHECK"].Value);
+               DataTable dt = new DataTable();
+               sql1.Fill(dt);
+               sql1.Dispose();
+               return dt;
+
+          }
+     }
 }
