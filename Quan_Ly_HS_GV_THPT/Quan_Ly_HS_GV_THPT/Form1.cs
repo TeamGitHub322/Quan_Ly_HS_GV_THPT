5b69325b13e81c58c66579ba67f30386518eb0e6
Nguyễn Đình Việt <70870418+Nguyen-Dinh-Viet@users.noreply.github.com>
2020-11-27 10:09:19 +0700

    Form Start QL_HS_GV_THPT

---
+++ Quan_Ly_HS_GV_THPT/Quan_Ly_HS_GV_THPT/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Ly_HS_GV_THPT
+{
+    public partial class frm_Start : Form
+    {
+        public frm_Start()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Start_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
